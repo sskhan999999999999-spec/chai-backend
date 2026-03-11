64a7400a4932ac4f0d3529b82ce40ab62ee17d7b
sskhan999999999999@gmail.com <sskhan@gmail.com>
2026-03-11 16:07:56 +0500

    connect  to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:"./env"
+})
+connectDB()
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express()
+
+(async() => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("Error",(error)=>{
+            console.log("Error", error);
+            throw error            
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.error("Error: ", error)
+        throw error
+    }
+})()*/
